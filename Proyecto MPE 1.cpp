5021cc2f386eb3301107868835f1f738d30fd6da
Nestor <nestor.gar22194@gmail.com>
2010-05-06 12:02:05 -0430

    Añadido: Lectura por .txt para los ejemplos de cada vocal, lectura por .txt para una vocal como entrada, escritura en .txt para guardar los pesos de las neuronas de la capa, lectura del .txt para cargar los pesos (base de conocimiento) de cada neurona entrenada con anterioridad. Modificado: Clase Capa y Clase Perceptron, para añadir una sobrecarga de sus constructures, ahora puede recibir una matriz de pesos o iniciarlo aleatoriamente

--- Proyecto MPE 1.cpp
+++ Proyecto MPE 1.cpp
@@ -11,14 +11,170 @@ using namespace std;
 #define NUM_FILAS 16
 #define NUM_COLUMNAS 10
 
+vector<int> getResultadoEsperadoDeNombreDeArchivo(string filepath){
+	if(filepath == "./a.txt"){
+		return {1,0,0,0,0};
+	}
+	else if(filepath == "./e.txt"){
+		return {0,1,0,0,0};
+	}
+	else if(filepath == "./i.txt"){
+		return {0,0,1,0,0};
+	}
+	else if(filepath == "./o.txt"){
+		return {0,0,0,1,0};
+	}
+	else if(filepath == "./u.txt"){
+		return {0,0,0,0,1};
+	}
+}
+
+vector<pair<vector<vector<int>>, vector<int>>> leerEjemplosUnaLetra(string filepath){
+	vector<pair<vector<vector<int>>, vector<int>>> ER;
+	vector<vector<int>> matriz;
+	
+	ifstream archivo_entrada;
+	archivo_entrada.open(filepath);
+	string linea;
+	int i , j = 0;
+	while(getline(archivo_entrada, linea)){
+		if(linea != "&"){
+			stringstream ss (linea);
+			string digito;
+			j = 0;
+			vector<int> fila;
+			while(std::getline(ss, digito, ' ')){
+				fila.push_back(stoi(digito)); 
+				j++;
+			}
+			matriz.push_back(fila);
+			i++;
+		}else{
+			i = 0;
+			vector<int> vector_esperado = getResultadoEsperadoDeNombreDeArchivo(filepath);
+			pair<vector<vector<int>>, vector<int>> par(matriz, vector_esperado);
+			ER.push_back(par);
+			matriz.clear();
+		}
+	}
+	archivo_entrada.close();
+	return ER;
+}
+
+vector<pair<vector<vector<int>>, vector<int>>> leerEjemplos(){
+	vector<pair<vector<vector<int>>, vector<int>>> ERa = leerEjemplosUnaLetra("./a.txt");
+	vector<pair<vector<vector<int>>, vector<int>>> ERe = leerEjemplosUnaLetra("./e.txt");
+	vector<pair<vector<vector<int>>, vector<int>>> ERi = leerEjemplosUnaLetra("./i.txt");
+	vector<pair<vector<vector<int>>, vector<int>>> ERo = leerEjemplosUnaLetra("./o.txt");
+	vector<pair<vector<vector<int>>, vector<int>>> ERu = leerEjemplosUnaLetra("./u.txt");
+	
+	vector<pair<vector<vector<int>>, vector<int>>> ER = {};
+	
+	ER.insert(ER.end(), ERa.begin(), ERa.end());
+	ER.insert(ER.end(), ERe.begin(), ERe.end());
+	ER.insert(ER.end(), ERi.begin(), ERi.end());
+	ER.insert(ER.end(), ERo.begin(), ERo.end());
+	ER.insert(ER.end(), ERu.begin(), ERu.end());
+	
+	return ER;
+}
+
+vector<vector<int>> leerEntrada(){
+	vector<vector<int>> matriz;
+	
+	ifstream archivo_entrada;
+	archivo_entrada.open("./entrada.txt");
+	string linea;
+	while(getline(archivo_entrada, linea)){
+		if(linea != "&"){
+			stringstream ss (linea);
+			string digito;
+			vector<int> fila;
+			while(std::getline(ss, digito, ' ')){
+				fila.push_back(stoi(digito)); 
+			}
+			matriz.push_back(fila);
+		}
+	}
+	archivo_entrada.close();
+	return matriz;
+}
+
+void escribirPesos(vector<vector<vector<double>>> matrices_pesos){
+	ofstream archivo_salida;
+	archivo_salida.open("./pesos.txt");
+	
+	vector<vector<double>> matriz;
+	for(int n = 0; n < matrices_pesos.size(); n++){
+		matriz = matrices_pesos[n];
+		for(int i = 0;  i < matriz.size(); i++){
+			for(int j = 0; j < matriz[i].size(); j++){
+				archivo_salida << matriz[i][j];
+				if(j != (matriz[i].size() - 1)){
+					archivo_salida <<  " ";
+				}
+			}
+			archivo_salida << endl;
+		}
+		archivo_salida << '&' << endl;	
+	}
+	
+	archivo_salida.close();
+}
+
+vector<vector<vector<double>>> leerPesos(){
+	vector<vector<vector<double>>> pesos;
+	vector<vector<double>> matriz;
+	
+	ifstream archivo_entrada;
+	archivo_entrada.open("./pesos.txt");
+	string linea;
+	while(getline(archivo_entrada, linea)){
+		if(linea != "&" && linea != ""){
+			stringstream ss (linea);
+			string digito;
+			vector<double> fila;
+			while(std::getline(ss, digito, ' ')){
+				fila.push_back(stod(digito));
+			}
+			matriz.push_back(fila);
+		}else{
+			pesos.push_back(matriz);
+			matriz.clear();
+		}
+	}
+	archivo_entrada.close();
+	return pesos;
+}
+
+void resultadoFinal(int pos_max){
+	switch (pos_max){
+		case 0:
+			cout << "Es una a." << endl;
+			break;
+		case 1:
+			cout << "Es una e." << endl;
+			break;
+		case 2:
+			cout << "Es una i." << endl;
+			break;
+		case 3:
+			cout << "Es una o." << endl;
+			break;
+		case 4:
+			cout << "Es una u." << endl;
+			break;
+		}
+}
+
 class Perceptron{
 	double umbral;
 	double pesos [NUM_FILAS][NUM_COLUMNAS];
 	double razon_aprendizaje;
-	// Añadir: Valor de aprendizaje
 	
 	public:
 		Perceptron(double u, double r);
+		Perceptron(double u, double r, vector<vector<double>> matriz_pesos);
 		
 		double suma(vector<vector<int>> entrada){
 			double sumatoria = 0;
@@ -62,6 +218,19 @@ class Perceptron{
 		double get_umbral(){
 			return umbral;
 		}
+		
+		vector<vector<double>> getPesos(){
+			vector<vector<double>> matriz_pesos;
+			vector<double> fila;
+			for(int i = 0; i < NUM_FILAS; i++){
+				for(int j = 0; j < NUM_COLUMNAS; j++){
+					fila.push_back(pesos[i][j]);
+				}
+				matriz_pesos.push_back(fila);
+				fila.clear();
+			}
+			return matriz_pesos;
+		}
 };
 
 Perceptron::Perceptron(double u, double r){
@@ -76,7 +245,16 @@ Perceptron::Perceptron(double u, double r){
 	}
 }
 
-
+Perceptron::Perceptron(double u, double r, vector<vector<double>> matriz_pesos){
+	umbral = u;
+	razon_aprendizaje = r;
+	
+	for(int i = 0; i < NUM_FILAS; i++){
+		for(int j = 0; j < NUM_COLUMNAS; j++){
+			pesos[i][j] = matriz_pesos[i][j];
+		}
+	}
+}
 
 class Capa{
 	int numero_neuronas;
@@ -86,6 +264,7 @@ class Capa{
 	
 	public:
 		Capa(int numero_neuronas);
+		Capa(int numero_neuronas, vector<vector<vector<double>>> lista_matrices_pesos);
 		
 	void procesar(vector<vector<int>> entrada){
 		vector<double> nuevo_resultado;
@@ -137,7 +316,7 @@ class Capa{
 	
 	void entrenar(vector<pair<vector<vector<int>>, vector<int>>> ER){
 		int pos;
-		for(int n = 0; n < 500; n++){
+		for(int n = 0; n < 1000; n++){
 			pos = rand() % (ER.size()) + 0;
 			procesar(ER[pos].first);
 			for(int i = 0; i < 4; i++){
@@ -155,6 +334,15 @@ class Capa{
 	vector<double> getResultado(){
 		return resultado;
 	}
+	
+	void guardarBaseConocimientos(){
+		vector<vector<vector<double>>> lista_pesos;
+		for(int n = 0; n < numero_neuronas; n++){
+			lista_pesos.push_back(neuronas[n].getPesos());
+		}
+		
+		escribirPesos(lista_pesos);
+	}
 };
 
 Capa::Capa(int n){
@@ -166,351 +354,39 @@ Capa::Capa(int n){
 	}
 }
 
-vector<int> getResultadoEsperadoDeNombreDeArchivo(string filepath){
-	if(filepath == "./a.txt"){
-		return {1,0,0,0,0};
-	}
-	else if(filepath == "./e.txt"){
-		return {0,1,0,0,0};
-	}
-	else if(filepath == "./i.txt"){
-		return {0,0,1,0,0};
-	}
-	else if(filepath == "./o.txt"){
-		return {0,0,0,1,0};
-	}
-	else if(filepath == "./u.txt"){
-		return {0,0,0,0,1};
-	}
-}
-
-vector<pair<vector<vector<int>>, vector<int>>> leerEjemplos(string filepath){
-	vector<pair<vector<vector<int>>, vector<int>>> ER;
-	vector<vector<int>> matriz;
+Capa::Capa(int n, vector<vector<vector<double>>> lista_matrices_pesos){
+	numero_neuronas = n;
 	
-	ifstream archivo_entrada;
-	archivo_entrada.open(filepath);
-	string linea;
-	int i , j = 0;
-	while(getline(archivo_entrada, linea)){
-		if(linea != "&"){
-			stringstream ss (linea);
-			string digito;
-			j = 0;
-			vector<int> fila;
-			while(std::getline(ss, digito, ' ')){
-				fila.push_back(stoi(digito)); 
-				j++;
-			}
-			matriz.push_back(fila);
-			i++;
-		}else{
-			i = 0;
-			vector<int> vector_esperado = getResultadoEsperadoDeNombreDeArchivo(filepath);
-			pair<vector<vector<int>>, vector<int>> par(matriz, vector_esperado);
-			ER.push_back(par);
-			matriz.clear();
-		}
+	for(int i = 0; i < numero_neuronas; i++){
+		Perceptron nueva_neurona = Perceptron(0.0, 0.07, lista_matrices_pesos[i]);
+		neuronas.push_back(nueva_neurona);
 	}
-	return ER;
-}
-
-
-void resultadoFinal(int pos_max){
-	switch (pos_max){
-		case 0:
-			cout << "Es una a." << endl;
-			break;
-		case 1:
-			cout << "Es una e." << endl;
-			break;
-		case 2:
-			cout << "Es una i." << endl;
-			break;
-		case 3:
-			cout << "Es una o." << endl;
-			break;
-		case 4:
-			cout << "Es una u." << endl;
-			break;
-		}
 }
 
 int main(){
-	srand(time(NULL));
-	//srand(42);
+	//srand(time(NULL));
+	srand(42);
 	
-	vector<vector<int>> entrada_a = 
-	{
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,1,1,1,1,0,0,0},
-	{0,0,1,1,1,1,1,1,0,0},
-	{0,1,1,1,0,0,1,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,1,0,0,1,1,1,0},
-	{0,0,1,1,1,1,1,1,1,0},
-	{0,0,0,1,1,1,1,0,1,1},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0}
-	};
+	ifstream archivo_pesos;
+	archivo_pesos.open("./pesos.txt");
 	
-	
-	vector<vector<int>> entrada_e = 
-	{
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,1,1,1,1,0,0,0},
-	{0,0,1,1,1,1,1,1,0,0},
-	{0,0,1,1,0,0,1,1,0,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,1,1,1,1,1,1,0},
-	{0,1,1,0,0,0,0,0,0,0},
-	{0,1,1,0,0,0,0,0,0,0},
-	{0,1,1,1,0,0,0,0,0,0},
-	{0,0,1,1,1,1,1,1,0,0},
-	{0,0,0,1,1,1,1,1,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0}
-	};
-	
-	vector<vector<int>> entrada_i = 
-	{
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,1,1,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0}
-	};
-	
-	vector<vector<int>> entrada_o = 
-	{
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,1,1,1,1,0,0,0},
-	{0,0,1,1,1,1,1,1,0,0},
-	{0,1,1,1,0,0,1,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,1,0,0,1,1,1,0},
-	{0,0,1,1,1,1,1,1,0,0},
-	{0,0,0,1,1,1,1,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0}
-	};
-	
-	vector<vector<int>> entrada_u = 
-	{
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,0,0,0,0,1,1,0},
-	{0,1,1,1,0,0,1,1,1,0},
-	{0,0,1,1,1,1,1,1,1,0},
-	{0,0,0,1,1,1,1,0,1,1},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0}
-	};
-	
-	
-	vector<vector<int>> entrada_a_kevin =
-	{
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
-	{0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
-	{0, 0, 1, 1, 1, 1, 1, 1, 1, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 1, 1},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	};
-	
-	vector<vector<int>> entrada_e_kevin =
-	{
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
-	{0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-	{0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-	{0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-	{0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	};
-	
-	vector<vector<int>> entrada_i_kevin =
-	{
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	};
-	
-	vector<vector<int>> entrada_o_kevin =
-	{
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
-	{0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
-	{0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	};
-	
-	vector<vector<int>> entrada_u_kevin =
-	{
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
-	{0, 0, 1, 1, 1, 1, 1, 1, 1, 0},
-	{0, 0, 0, 1, 1, 1, 1, 0, 1, 1},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	};
-	/*
-	pair<vector<vector<int>>, vector<int>> par_a(entrada_a, {1,0,0,0,0});
-	pair<vector<vector<int>>, vector<int>> par_e(entrada_e, {0,1,0,0,0});
-	pair<vector<vector<int>>, vector<int>> par_i(entrada_i, {0,0,1,0,0});
-	pair<vector<vector<int>>, vector<int>> par_o(entrada_o, {0,0,0,1,0});
-	pair<vector<vector<int>>, vector<int>> par_u(entrada_u, {0,0,0,0,1});
-	pair<vector<vector<int>>, vector<int>> par_a_kevin(entrada_a_kevin, {1,0,0,0,0});
-	pair<vector<vector<int>>, vector<int>> par_e_kevin(entrada_e_kevin, {0,1,0,0,0});
-	pair<vector<vector<int>>, vector<int>> par_i_kevin(entrada_i_kevin, {0,0,1,0,0});
-	pair<vector<vector<int>>, vector<int>> par_o_kevin(entrada_o_kevin, {0,0,0,1,0});
-	pair<vector<vector<int>>, vector<int>> par_u_kevin(entrada_u_kevin, {0,0,0,0,1});
-	
-	
-	vector<pair<vector<vector<int>>, vector<int>>> ER =
-	{
-		par_a,
-		par_e,
-		par_i,
-		par_o,
-		par_u,
-		par_a_kevin,
-		par_e_kevin,
-		par_i_kevin,
-		par_o_kevin,
-		par_u_kevin
-	};
-	*/
-	
-	vector<pair<vector<vector<int>>, vector<int>>> vectorER;
-	vectorER = leerEjemplos("./a.txt");
-	
-	vector<vector<int>> matriz = vectorER[0].first;
-	//cout << "Juan";
-	for(int i = 0; i < NUM_FILAS; i++){
-		//cout << "Jose";
-		for(int j = 0; j < NUM_COLUMNAS; j++){
-			//cout << "Velazquez";
-			cout << matriz[i][j] << " ";
-			//cout << "Lares";
-		}
-		cout << endl;
+	if(archivo_pesos.is_open()){
+		archivo_pesos.close();
+		vector<vector<vector<double>>> lista_pesos = leerPesos();
+		Capa capa = Capa(5, lista_pesos);
+		vector<vector<int>> vocal = leerEntrada();
+		capa.procesar(vocal);
+		resultadoFinal(capa.getMaximoResultado());
+	}else{
+		archivo_pesos.close();
+		Capa capa = Capa(5);
+		vector<pair<vector<vector<int>>, vector<int>>> ER = leerEjemplos();
+		capa.entrenar(ER);
+		vector<vector<int>> vocal = leerEntrada();
+		capa.procesar(vocal);
+		resultadoFinal(capa.getMaximoResultado());
+		capa.guardarBaseConocimientos();
 	}
-	
-	cout << endl;
-	
-	vector<int> salidaEsperada = vectorER[0].second;
-	for(int i = 0; i < 5; i++){
-		cout << salidaEsperada[i] << " ";
-	}
-	
-	cout << endl;
-	
-	matriz = vectorER[1].first;
-	//cout << "Juan";
-	for(int i = 0; i < NUM_FILAS; i++){
-		//cout << "Jose";
-		for(int j = 0; j < NUM_COLUMNAS; j++){
-			//cout << "Velazquez";
-			cout << matriz[i][j] << " ";
-			//cout << "Lares";
-		}
-		cout << endl;
-	}
-	
-	cout << endl;
-	
-	salidaEsperada = vectorER[1].second;
-	for(int i = 0; i < 5; i++){
-		cout << salidaEsperada[i] << " ";
-	}
-	
+
 	return 0;
 }
