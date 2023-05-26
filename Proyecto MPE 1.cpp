87cb423ba21b4275c0a18d0877f0c643d52df5f5
Nestor <nestor.gar22194@gmail.com>
2023-05-25 21:11:45 -0400

    Añadido: Documentación. Modificado: Semilla aleatoria, pasa de ser 42 de forma estatica a estar en funcion del tiempo. Funcion de entrenamiento de la Red, ahora adapta su numero de iteraciones al tamaño de la lista de ejemplos que reciba

--- Proyecto MPE 1.cpp
+++ Proyecto MPE 1.cpp
@@ -11,6 +11,9 @@ using namespace std;
 #define NUM_FILAS 16
 #define NUM_COLUMNAS 10
 
+/*FUNCIONES AUXILIARES*/
+
+//Función para retornar la salida esperada, en base al nombre de un archivo.txt
 vector<int> getResultadoEsperadoDeNombreDeArchivo(string filepath){
 	if(filepath == "./a.txt"){
 		return {1,0,0,0,0};
@@ -27,8 +30,11 @@ vector<int> getResultadoEsperadoDeNombreDeArchivo(string filepath){
 	else if(filepath == "./u.txt"){
 		return {0,0,0,0,1};
 	}
+	//Perdón, es que el switch de C++ no acepta cadenas
 }
 
+/*Función para retornar todas las vocales contenidas en un archivo .txt, así como
+la salida esperada de dicha vocal*/
 vector<pair<vector<vector<int>>, vector<int>>> leerEjemplosUnaLetra(string filepath){
 	vector<pair<vector<vector<int>>, vector<int>>> ER;
 	vector<vector<int>> matriz;
@@ -36,21 +42,22 @@ vector<pair<vector<vector<int>>, vector<int>>> leerEjemplosUnaLetra(string filep
 	ifstream archivo_entrada;
 	archivo_entrada.open(filepath);
 	string linea;
-	int i , j = 0;
 	while(getline(archivo_entrada, linea)){
 		if(linea != "&"){
 			stringstream ss (linea);
 			string digito;
-			j = 0;
 			vector<int> fila;
+			
+			/*getline(ss, digito, ' ') separa la linea por espacios, y copia los valores separados en
+			la variable digito*/
 			while(std::getline(ss, digito, ' ')){
 				fila.push_back(stoi(digito)); 
-				j++;
+				
 			}
+			
 			matriz.push_back(fila);
-			i++;
+			
 		}else{
-			i = 0;
 			vector<int> vector_esperado = getResultadoEsperadoDeNombreDeArchivo(filepath);
 			pair<vector<vector<int>>, vector<int>> par(matriz, vector_esperado);
 			ER.push_back(par);
@@ -61,6 +68,8 @@ vector<pair<vector<vector<int>>, vector<int>>> leerEjemplosUnaLetra(string filep
 	return ER;
 }
 
+/*Función para retornar todas las vocales contenidas en todos los archivos .txt.
+Básicamente concatena los resultados de la función anterior*/
 vector<pair<vector<vector<int>>, vector<int>>> leerEjemplos(){
 	vector<pair<vector<vector<int>>, vector<int>>> ERa = leerEjemplosUnaLetra("./a.txt");
 	vector<pair<vector<vector<int>>, vector<int>>> ERe = leerEjemplosUnaLetra("./e.txt");
@@ -76,9 +85,11 @@ vector<pair<vector<vector<int>>, vector<int>>> leerEjemplos(){
 	ER.insert(ER.end(), ERo.begin(), ERo.end());
 	ER.insert(ER.end(), ERu.begin(), ERu.end());
 	
+	
 	return ER;
 }
-
+/*Función para leer la vocal que se encuentra en el archivo "entrada.txt"
+la cual es la vocal que el usuario desea que la red reconozca*/
 vector<vector<int>> leerEntrada(){
 	vector<vector<int>> matriz;
 	
@@ -100,6 +111,9 @@ vector<vector<int>> leerEntrada(){
 	return matriz;
 }
 
+/*Función para guardar las matrices de pesos en un archvo "pesos.txt"
+el cual actuará como la base de conocimientos para las siguientes
+ejecuciones del programa */
 void escribirPesos(vector<vector<vector<double>>> matrices_pesos){
 	ofstream archivo_salida;
 	archivo_salida.open("./pesos.txt");
@@ -122,6 +136,7 @@ void escribirPesos(vector<vector<vector<double>>> matrices_pesos){
 	archivo_salida.close();
 }
 
+/*Función para leer las matrices de pesos guardadas en el archivo "pesos.txt"*/
 vector<vector<vector<double>>> leerPesos(){
 	vector<vector<vector<double>>> pesos;
 	vector<vector<double>> matriz;
@@ -144,9 +159,15 @@ vector<vector<vector<double>>> leerPesos(){
 		}
 	}
 	archivo_entrada.close();
+	cout << "Base de conocimiento cargada exitosamente" << endl;
 	return pesos;
 }
 
+/*Función para dar el resultado final al usuario.
+Básicamente es la interfaz de salida, funciona recibiendo un número
+entre 0 y 4, que representa la posicion de la neurona que tuvo el
+resultado más alto luego de procesar la entrada. A partir de esto
+un print informa al usuario de la conclusión de la red. */
 void resultadoFinal(int pos_max){
 	switch (pos_max){
 		case 0:
@@ -167,6 +188,9 @@ void resultadoFinal(int pos_max){
 		}
 }
 
+/*Clase Perceptrón (o Neurona), su atributo más importante es el de los pesos,
+el cual es una matriz de 16x10. Cada celda de esta matriz está asociada con un
+digito de la entrada (o un pixel si fuera una imagen)*/
 class Perceptron{
 	double umbral;
 	double pesos [NUM_FILAS][NUM_COLUMNAS];
@@ -204,21 +228,6 @@ class Perceptron{
 			}
 		}
 		
-		void toStringPesos(){
-			cout << "================================================================================" << endl;
-			for(int i = 0; i < NUM_FILAS; i++){
-				for(int j = 0; j < NUM_COLUMNAS; j++){
-					cout << pesos[i][j] << " ";
-				}
-				cout << endl;
-			}
-			cout << "================================================================================" << endl;
-		}
-		
-		double get_umbral(){
-			return umbral;
-		}
-		
 		vector<vector<double>> getPesos(){
 			vector<vector<double>> matriz_pesos;
 			vector<double> fila;
@@ -233,18 +242,24 @@ class Perceptron{
 		}
 };
 
+/*La clase Perceptrón tiene dos constructores.
+El primero construye el Perceptrón con su matriz de pesos
+llena de número aleatorios*/
 Perceptron::Perceptron(double u, double r){
 	umbral = u;
 	razon_aprendizaje = r;
 	
 	for(int i = 0; i < NUM_FILAS; i++){
 		for(int j = 0; j < NUM_COLUMNAS; j++){
+			//Genera un número aleatorio real entre -1 y 1
 			pesos[i][j] = -1.0 + (double)(rand()) / ((double) RAND_MAX/(1 - (-1)));
-			//((double) rand())/RAND_MAX;
 		}
 	}
 }
 
+/*El segundo construye el Perceptrón a partir de una
+matriz de pesos recibida por parámetro. De esta forma, el perceptrón
+recupera el conocimiento adquirido en las ejecuciones previas*/
 Perceptron::Perceptron(double u, double r, vector<vector<double>> matriz_pesos){
 	umbral = u;
 	razon_aprendizaje = r;
@@ -256,6 +271,13 @@ Perceptron::Perceptron(double u, double r, vector<vector<double>> matriz_pesos){
 	}
 }
 
+/*Clase Capa, sus atributos más importantes son:
+1. Una lista de Perceptrones, que representan la capa en sí
+2. Una lista de resultados, que recopila el resultado de la función de activación
+de cada neurona en la capa
+3. Una lista de salida, que tiene un 1 en la posicion del perceptrón cuya
+función de activación tuvo un resultado más alto, y tiene 0's en todas las demás
+posiciones*/
 class Capa{
 	int numero_neuronas;
 	vector<Perceptron> neuronas;
@@ -315,8 +337,17 @@ class Capa{
 	
 	
 	void entrenar(vector<pair<vector<vector<int>>, vector<int>>> ER){
+		cout << "Iniciando entrenamiento" << endl;
 		int pos;
-		for(int n = 0; n < 1000; n++){
+		int iteraciones = 500;
+		/*El entrenamiento se va a realizar 500 veces, o una cantidad
+		de veces igual la longitud de la lista de Pares Entrada-Resultados
+		multiplcada por 10, lo que sea mayor*/
+		if(iteraciones < (ER.size()*10)){
+			iteraciones = ER.size()*10;
+		}
+		
+		for(int n = 0; n < iteraciones; n++){
 			pos = rand() % (ER.size()) + 0;
 			procesar(ER[pos].first);
 			for(int i = 0; i < 4; i++){
@@ -325,6 +356,8 @@ class Capa{
 				}
 			}
 		}
+		cout << "Entrenamiento terminado. ";
+		cout << "Se guardara la base de conocimiento adquirida al terminar el programa" << endl;
 	}
 	
 	vector<Perceptron> getNeuronas(){
@@ -345,6 +378,8 @@ class Capa{
 	}
 };
 
+/*La Clase Capa tiene dos constructores.
+El primero inicia los pesos de cada uno de sus perceptrones aleatoriamente*/
 Capa::Capa(int n){
 	numero_neuronas = n;
 	
@@ -354,6 +389,9 @@ Capa::Capa(int n){
 	}
 }
 
+/*El segundo inicia los pesos de cada uno de sus perceptrones acorde
+a los contenidos en el archivo "pesos.txt", recuperando la base de
+conocimiento de la red*/
 Capa::Capa(int n, vector<vector<vector<double>>> lista_matrices_pesos){
 	numero_neuronas = n;
 	
@@ -364,9 +402,50 @@ Capa::Capa(int n, vector<vector<vector<double>>> lista_matrices_pesos){
 }
 
 int main(){
-	//srand(time(NULL));
-	srand(42);
+	/*
+	Realizado por Nestor Aguilar (C.I: 28.316.308)
 	
+	Este programa requiere de dos elementos básicos para funcionar correctamente:
+	
+	1. Un archivo "entrada.txt" en su mismo directorio el cual contiene una matriz
+	de 16x10, que contiene 0's y 1's, y representa la vocal que desea la red intente
+	reconocer. Si desea cambiar la vocal que la red neuronal intenta reconocer, basta
+	con sustituir la matriz que se encuentra en el archivo "entrada.txt" por la matriz 
+	que represente la vocal que desea, y ejecutar el programa nuevamente.
+	
+	2. Un archivo "<vocal>.txt" en su mismo directorio por cada vocal, que contiene 
+	matrices 16x10, que contienen 0's y 1's, y representan la vocal correspondiente 
+	a su nombre de archivo (es decir "a.txt" contiene matrices que representan vocales a,
+	"e.txt" contiene matrices que representan vocales e, y así sucesivamente), cada matriz
+	terminada en un ampersand (&). Estos archivos son importantes porque se usarán como 
+	ejemplo para el entrenamiento de la red neuronal. En la entrega del proyecto se
+	suministraron archivos .txt para cada vocal con 15 o 16 ejemplos cada uno. Si desea 
+	añadir o eliminar ejemplos para hacer un entrenamiento más o menos exhaustivo, 
+	basta con añadir o eliminar dichas matrices de sus archivos correspondientes. 
+	No olvide colocar un ampersand (&) al final.
+	
+	
+	El flujo del programa es el siguiente:
+	
+	1. Buscará un archivo "pesos.txt" en el mismo directorio. Este archivo representa
+	la base de conocimientos de la red neuronal.
+	
+	2. Si lo encuentra, procederá a cargar el archivo y crear la red neuronal con la
+	base de conocimientos, para luego procesar la vocal contenida en el archivo "entrada.txt".
+	
+	3. Si no lo encuentra, procederá a crear la red neuronal con un estado inicial
+	aleatorio, luego procederá a cargar los ejemplos y entrenar a la red con ellos.
+	Una vez el entrenamiento haya terminado, procesará la vocal contenida en el archivo 
+	"entrada.txt". Finalmente, guardará los pesos adquiridos luego del entrenamiento
+	en un archivo "pesos.txt" recién creado, para las siguientes ejecuciones.
+	
+	En la entrega del proyecto, se suministró un archivo "pesos.txt" con la última
+	base de conocimientos de la red, por lo que la primera ejecución de programa trabajará
+	en base a estos conocimientos. Si desea probar el entrenamiento de la red neuronal,
+	basta con eliminar el archivo "pesos.txt" del directorio.
+	*/	
+	
+	srand(time(NULL));
 	ifstream archivo_pesos;
 	archivo_pesos.open("./pesos.txt");
 	
@@ -375,6 +454,7 @@ int main(){
 		vector<vector<vector<double>>> lista_pesos = leerPesos();
 		Capa capa = Capa(5, lista_pesos);
 		vector<vector<int>> vocal = leerEntrada();
+		cout << "Procesando vocal..." << endl;
 		capa.procesar(vocal);
 		resultadoFinal(capa.getMaximoResultado());
 	}else{
@@ -383,10 +463,14 @@ int main(){
 		vector<pair<vector<vector<int>>, vector<int>>> ER = leerEjemplos();
 		capa.entrenar(ER);
 		vector<vector<int>> vocal = leerEntrada();
+		cout << "Procesando vocal..." << endl;
 		capa.procesar(vocal);
 		resultadoFinal(capa.getMaximoResultado());
 		capa.guardarBaseConocimientos();
 	}
 
+	/*La Red Neuronal se llama "IAbdul"
+	Salude a IAbdul antes de que se acabe su ejecución...*/
 	return 0;
+	/*Adiós IAbdul*/
 }
